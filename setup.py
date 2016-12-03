64f9bcfb1d0b356b153df2d804c09e06a66aeda4
H-Plus-Time <nicholas.roberts.au@gmail.com>
2016-12-03 19:31:40 +1100

    Completed package structure, should be installable via pip now

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='newport-xps-q8',
+setup(name='newportXpsQ8',
       version='0.1',
       description='driver, utils for Newport XPS Q8',
       url='https://github.com/H-Plus-Time/newport-xps-q8',
       author='H-Plus-Time',
       author_email='hplustime@gmail.com',
       license='MIT',
-      packages=['newport-xps-q8'],
+      packages=['newportXpsQ8'],
       zip_safe=False)
